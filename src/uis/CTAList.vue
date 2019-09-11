28fab95d3de590aa76e629078adab2e7f2263fbf
wonglok <yellowhappy831@gmail.com>
2019-09-12 06:24:30 +0800

    front-end-deploy

--- src/uis/CTAList.vue
+++ src/uis/CTAList.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="h-full w-56 xl:w-64 border-l border-gray-600 scrolling-touch overflow-y-auto">
+
+    <a class="text-xl font-serif border-b border-gray-600 py-6 text-center select-none hover:bg-gray-200 cursor-pointer flex items-center justify-center h-64" target="_blank" href="https://effectnode.com">
+      Effect Node
+    </a>
+    <a class="text-xl font-serif border-b border-gray-600 py-6 text-center select-none hover:bg-gray-200 cursor-pointer flex items-center justify-center h-64" target="_age" href="https://age.wonglok.com">
+      Assisted Graphics Engineering.
+    </a>
+    <a class="text-xl font-serif border-b border-gray-600 py-6 text-center select-none hover:bg-gray-200 cursor-pointer flex items-center justify-center h-64" target="_blank" href="https://igraph.effectnode.com">
+      iGraph Cinematics
+    </a>
     <div class="text-xl font-serif border-b border-gray-600 py-6 text-center select-none hover:bg-gray-200 cursor-pointer flex items-center justify-center h-64" @click="bamboozo">
       Click Bait.
     </div>
