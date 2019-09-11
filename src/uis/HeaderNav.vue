c13a151ed06f291aab02987d3f5834f5e8a1f3ca
wonglok <yellowhappy831@gmail.com>
2019-09-11 18:07:44 +0800

    front-end-deploy

--- src/uis/HeaderNav.vue
+++ src/uis/HeaderNav.vue
@@ -7,7 +7,7 @@
       </svg>
     </div>
     <div class=" select-none cursor-pointer">
-      <img src="../img/logo.svg" alt="">
+      <img src="../img/logo.svg" alt="Logo">
     </div>
     <div class="block md:hidden absolute mobile-menu w-12 h-12 p-4 cursor-pointer" @click="overlay = 'menu'">
       <svg class="w-9" width="39" height="16" viewBox="0 0 39 16" fill="none" v-if="overlay !== 'menu'" xmlns="http://www.w3.org/2000/svg">
@@ -22,13 +22,13 @@
       <div class="relative h-20 flex justify-center md:justify-between items-center border-b border-gray-600 text-xl">
         <div class="hidden md:flex px-5 border-r border-gray-600 h-full flex items-center justify-center cursor-pointer select-none hover:bg-gray-200 font-serif w-56 xl:w-64 text-center items-center" @click="overlay = false">
           <svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <line x1="28.3448" y1="1.36207" x2="0.344828" y2="28.0287" stroke="black"/>
-          <line x1="0.344828" y1="0.637931" x2="28.3448" y2="27.3046" stroke="black"/>
+            <line x1="28.3448" y1="1.36207" x2="0.344828" y2="28.0287" stroke="black"/>
+            <line x1="0.344828" y1="0.637931" x2="28.3448" y2="27.3046" stroke="black"/>
           </svg>
 
         </div>
-        <div class=" select-none cursor-pointer">
-          <img src="../img/menu.svg" alt="">
+        <div class=" select-none">
+          <img src="../img/menu.svg" alt="Menu">
         </div>
         <div class="block md:hidden absolute mobile-close-menu w-12 h-12 p-4 cursor-pointer" @click="overlay = false">
           <svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -38,7 +38,7 @@
         </div>
         <div class="hidden md:flex px-5 border-l border-gray-600 h-full flex items-center justify-center cursor-pointer select-none hover:bg-gray-200 font-serif w-56 xl:w-64 text-center items-center">Loving Gift 🥰</div>
       </div>
-      <div class="bg-gray-400">
+      <div class="bg-gray-400 fuller">
         OMG
       </div>
     </div>
@@ -61,12 +61,19 @@ export default {
 </script>
 
 <style scoped>
+.fuller {
+  height: calc(100% - 5rem);
+}
+
 .mobile-menu{
   top: 15px;
-  right: 30px;
+  right: 20px;
+  -webkit-tap-highlight-color: transparent;
 }
+
 .mobile-close-menu{
   top: 10px;
   right: 20px;
+  -webkit-tap-highlight-color: transparent;
 }
 </style>
